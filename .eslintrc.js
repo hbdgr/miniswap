8d8e883e4251919715b39aed5a73e645c913a5cd
hbdgr <hbdgr@protonmail.com>
2024-04-25 16:23:27 +0200

    miniswap: initial configuration

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  extends: [
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint",
+  ],
+  rules: {
+    // Add any custom rules or override the existing ones here
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "quote-props": "off",
+    "space-before-function-paren": [
+      "error", {
+        "anonymous": "always",
+        "named": "ignore",
+        "asyncArrow": "always",
+      },
+    ],
+  },
+};
---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
+
+import dotenv from "dotenv";
+
+const reportGas = process.env.REPORT_GAS?.toLowerCase() === "true";
+const reportSize = process.env.REPORT_SIZE?.toLowerCase() === "true";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.WALLET_MNEMONIC,
+      },
+    },
+    local: {
+      url: process.env.LOCAL_RPC_URL,
+      chainId: 31337,
+      accounts: {
+        mnemonic: process.env.WALLET_MNEMONIC,
+      },
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: {
+        mnemonic: process.env.WALLET_MNEMONIC,
+      },
+      chainId: 11155111,
+    },
+  },
+
+  gasReporter: {
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_KEY,
+    enabled: reportGas,
+  },
+
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: reportSize,
+    strict: true,
+  },
+};
+
+export default config;
